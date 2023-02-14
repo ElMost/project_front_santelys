3082571db150266ebdd5ecbdda12d1cadf29f8a9
Mouss <elmost777@gmail.com>
2023-02-14 17:13:13 +0100

    ajout du logo dans la navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+// import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
         <h1>test</h1>{' '}
       <Routes>
       </Routes>
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import logo from '../../assets/logo.png';
 
 const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="logo" width={100} height={100} />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
